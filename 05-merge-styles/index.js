8cf9cf7927d13e6f30c1a94eecd8f3bace9ca3b3
Daniil Sharenkov <Milkiy174@gmail.com>
2022-05-16 17:28:37 +0300

    feat: make 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const readPath = path.join(__dirname, 'styles');
+let distWriteStream = fs.createWriteStream(bundlePath);
+// const distPath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+fs.readdir(readPath, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    for (let i = 0; i < files.length; i++) {
+      const index = files[i].indexOf('.');
+      if (files[i].slice(index + 1, files[i].length) === 'css') {
+        const newReadPath = path.join(__dirname, 'styles', files[i]);
+        const readStream = fs.createReadStream(newReadPath);
+        readStream.on('data', (text) => {
+          distWriteStream.write(text);
+        });
+      }
+    }
+  }
+});
