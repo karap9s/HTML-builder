6d6ebe829c6fbaa8d7a65b182480f32db63d97ca
Daniil Sharenkov <Milkiy174@gmail.com>
2022-05-16 16:33:16 +0300

    feat: make 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyPath = path.join(__dirname, 'files');
+const newPath = path.join(__dirname, 'files-copy');
+
+fs.promises.mkdir(newPath, { recursive: true });
+
+fs.readdir(copyPath, (err, files) => {
+  if (err)
+    console.log(err);
+  for (let i = 0; i < files.length; i++) {
+    let newCopyPath = path.join(__dirname, 'files', files[i]);
+    let veryNewPath = path.join(__dirname, 'files-copy', files[i]);
+    fs.promises.copyFile(newCopyPath, veryNewPath);
+  }
+});
